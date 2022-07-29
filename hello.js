ef4562f6463619566a5666697959b39bf83eefb4
zhuicode99 <enlin291814833@hotmail.com>
2022-07-29 02:34:38 +0000

    update new files

---
+++ hello.js
@@ -0,0 +1,12 @@
+/*const sayHello  = function () {
+  console.log("Hello, world");
+}
+
+sayHello();  */
+
+//break
+
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHello("jason")
